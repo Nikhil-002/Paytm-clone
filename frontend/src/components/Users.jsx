748456456618542f635c6ab097b7d7319a04b6ff
Nikhil-002 <93790466+Nikhil-002@users.noreply.github.com>
2024-07-08 14:22:26 +0530

    Update Users.jsx

--- frontend/src/components/Users.jsx
+++ frontend/src/components/Users.jsx
@@ -13,7 +13,7 @@ export default function Users() {
 
   useEffect(() => {
     axios
-      .get("http://localhost:3000/api/v1/user/bulk?filter=" + filter)
+      .get("https://paytm-clone-server.vercel.app/api/v1/user/bulk?filter=" + filter)
       .then((response) => {
         setUsers(response.data.user);
         // console.log(response.data.user);
